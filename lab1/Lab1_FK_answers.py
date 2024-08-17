ec005c08bb50879e901b6096fd777af59bf64280
徐祖亮 <xzln1167@corp.netease.com>
2024-08-17 12:37:53 +0800

    complete lab1

--- lab1/Lab1_FK_answers.py
+++ lab1/Lab1_FK_answers.py
@@ -102,6 +102,24 @@ def part2_forward_kinematics(joint_name, joint_parent, joint_offset, motion_data
     return joint_positions, joint_orientations
 
 
+def truncate_joint_array(joint_name, joint_parent, joint_offset):
+    new_names = []
+    new_offsets = []
+    new_parent = []
+    id_map = {}
+    for i, name in enumerate(joint_name):
+        id_map[i] = len(new_names)
+        if name.endswith("_end"):
+            continue
+        new_names.append(name)
+        new_offsets.append(joint_offset[i])
+        if joint_parent[i] == -1:
+            new_parent.append(joint_parent[i])
+        else:
+            new_parent.append(id_map[joint_parent[i]])
+    return new_names, new_parent, new_offsets
+
+
 def part3_retarget_func(T_pose_bvh_path, A_pose_bvh_path):
     """
     将 A-pose的bvh重定向到T-pose上
@@ -112,5 +130,61 @@ def part3_retarget_func(T_pose_bvh_path, A_pose_bvh_path):
         两个bvh的joint name顺序可能不一致哦(
         as_euler时也需要大写的XYZ
     """
-    motion_data = None
+    joint_name_t, joint_parent_t, joint_offset_t = part1_calculate_T_pose(T_pose_bvh_path)
+    joint_name_a, joint_parent_a, joint_offset_a = part1_calculate_T_pose(A_pose_bvh_path)
+    joint_name_t, joint_parent_t, joint_offset_t = truncate_joint_array(joint_name_t, joint_parent_t, joint_offset_t)
+    joint_name_a, joint_parent_a, joint_offset_a = truncate_joint_array(joint_name_a, joint_parent_a, joint_offset_a)
+
+    zero_r = R([0, 0, 0, 1])
+    r_offsets = [zero_r for _ in range(len(joint_name_t))]
+    index_map = {}
+    for i, t_offset in enumerate(joint_offset_t):
+        joint_name = joint_name_t[i]
+        a_index = joint_name_a.index(joint_name)
+        index_map[i] = a_index
+        a_offset = joint_offset_a[a_index]
+
+        # 计算两个向量的点积
+        dot_product = np.dot(t_offset, a_offset)
+
+        # 计算两个向量的模长
+        norm_t = np.linalg.norm(t_offset)
+        norm_a = np.linalg.norm(a_offset)
+
+        cos_angle = min(dot_product / (norm_t * norm_a), 1.0)
+
+        # 计算夹角（弧度）
+        angle_rad = np.arccos(cos_angle)
+        if np.isnan(angle_rad):
+            continue
+
+        # 计算轴角旋转向量，即向量a和向量b的叉积，然后除以2得到旋转轴，再乘以夹角
+        axis = np.cross(t_offset / norm_t, a_offset / norm_a)
+        axis = axis / np.linalg.norm(axis)
+        if any(np.isnan(axis)):
+            continue
+        rotation_axis = axis * angle_rad
+        parent_r = R.from_rotvec(rotation_axis)
+        parent_id = joint_parent_t[i]
+        if parent_id != -1:
+            grand_parent_id = joint_parent_t[parent_id]
+            if grand_parent_id != -1:
+                parent_r = r_offsets[grand_parent_id].inv() * parent_r
+            r_offsets[parent_id] = parent_r
+
+    motion_data = load_motion_data(A_pose_bvh_path)
+    temp = []
+
+    for motion in motion_data:
+        motion_channel = motion.reshape(21, -1)
+        new_channel = motion_channel.copy()
+        for i in range(len(joint_name_t)):
+            t_index = i + 1
+            a_index = index_map[i] + 1
+            channel = motion_channel[a_index]
+            r = R.from_euler("XYZ", channel, degrees=True)
+            new_channel[t_index] = (r * r_offsets[t_index - 1]).as_euler("XYZ", degrees=True)
+            # print("t name", joint_name_t[t_index-1], "a name:", joint_name_a[a_index - 1], "before:", channel, "after:", (r * r_offsets[t_index - 1]).as_euler("XYZ", degrees=True))
+        temp.append(new_channel.reshape(1, -1))
+    motion_data = np.concatenate(temp, axis=0)
     return motion_data
